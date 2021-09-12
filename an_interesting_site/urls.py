28299de356c99b371fc900fc99fa7c7469d08012
Mikew1999 <mikewalters1234@gmail.com>
2021-09-12 21:47:46 +0000

    fixed problems with media in settings.py

--- an_interesting_site/urls.py
+++ an_interesting_site/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('allauth.urls')),
-]
+] + static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
