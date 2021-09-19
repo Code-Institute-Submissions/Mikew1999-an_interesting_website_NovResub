11c043500956a931c4a41319fc7db1dd5c388c64
Mikew1999 <mikewalters1234@gmail.com>
2021-09-19 18:29:42 +0000

    added category form to filter by category

--- products/views.py
+++ products/views.py
@@ -5,11 +5,19 @@ from .models import Products, Category
 def products(request):
     ''' A view to show all products '''
     products = Products.objects.all()
-    categories = Category.objects.all()
+    category_list = Category.objects.all()
+    selected = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            selected = categories
 
     context = {
         'products': products,
-        'categories': categories,
+        'categories': category_list,
+        'selected': selected,
     }
 
     return render(
