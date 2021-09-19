612f140e260612e5f2477b778235239d1c2def88
Mikew1999 <mikewalters1234@gmail.com>
2021-09-19 20:08:30 +0000

    rendered searched results, showed count of products returned and shows search criteria

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Products, Category
+from django.db.models import Q
 
 
 def products(request):
@@ -7,12 +8,20 @@ def products(request):
     products = Products.objects.all()
     category_list = Category.objects.all()
     selected = None
+    search = None
 
     if request.GET:
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
+            categories = request.GET.getlist('category')
             products = products.filter(category__name__in=categories)
-            selected = categories
+            selected = ', '.join(categories).capitalize()
+
+        if 'q' in request.GET:
+            search = request.GET['q']
+            results = Q(title__icontains=search) | Q(
+                category__name__icontains=search)
+            products = products.filter(results)
+            selected = str(search)
 
     context = {
         'products': products,
@@ -21,7 +30,7 @@ def products(request):
     }
 
     return render(
-                    request,
-                    'products/products.html',
-                    context
-                )
+        request,
+        'products/products.html',
+        context
+    )
