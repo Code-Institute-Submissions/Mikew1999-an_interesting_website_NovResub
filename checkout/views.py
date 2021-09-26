5428a8fa1be30ed33afeac093c431aab8baa9c3e
Mikew1999 <mikewalters1234@gmail.com>
2021-09-26 14:48:39 +0000

    rendered bag contents on checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render, redirect
 def checkout(request):
     ''' A view to return the checkout screen '''
     bag = request.session.get('bag', {})
+    user = request.user
+
     if bag == {}:
         return redirect(shopping_bag)
 
-    return render(request, 'checkout/checkout.html')
+    context = {
+        'user': user,
+    }
+
+    return render(request, 'checkout/checkout.html', context)
