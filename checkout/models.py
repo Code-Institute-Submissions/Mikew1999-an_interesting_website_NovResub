5595c813046691b7fa49219229f10c9712ba36ff
Mikew1999 <mikewalters1234@gmail.com>
2021-09-27 01:24:09 +0000

    created order model

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    ''' A model to define an order '''
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    address_line_1 = models.CharField(max_length=254, null=False, blank=False)
+    address_line_2 = models.CharField(max_length=254, null=True, blank=True)
+    town = models.CharField(max_length=70, null=False, blank=False)
+    post_code = models.CharField(max_length=8, null=False, blank=False)
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    phone = models.IntegerField(null=True, blank=True)
+    delivery_cost = models.DecimalField(max_digits=4, decimal_places=2, blank=False, null=False)
+    total = models.DecimalField(max_digits=1000000 ,decimal_places=2, blank=False, null=False)
+    items = models.CharField(max_length=100000, null=False, blank=False)
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from products.models import Products
+from checkout.models import Order
 
 
 def user_profile(request, user_id):
     ''' A view to return the users profile '''
     user = get_object_or_404(User, id=user_id)
+    product_count = Products.objects.filter(author=User(user_id)).count()
+    products = None
+    products = Products.objects.filter(author=User(user_id))[:4]
+    orders = Order.objects.filter(user=User(user_id))
+
+    if 'more' in request.GET:
+        products = Products.objects.filter(author=User(user_id))
 
     context = {
         'user': user,
+        'products': products,
+        'product_count': product_count,
+        'orders': orders,
     }
 
     return render(request, 'user_profile/user_profile.html', context)
