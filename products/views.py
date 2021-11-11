174fd1b2f8fef5d4e096de7517ccffdbce8a6739
Mikew1999 <mikewalters1234@gmail.com>
2021-11-11 00:28:22 +0000

    Fixed validation issues and removed redundant code from products.views, styled products on main products page.

--- products/views.py
+++ products/views.py
@@ -1,20 +1,24 @@
+''' Required imports '''
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.db.models import Q
+from reviews.calculate_reviews import CalculateRating
+from reviews.models import Review
 from .models import Products, Category, Likes
 from .forms import ProductForm
-from django.db.models import Q
-from django.contrib.auth.models import User
-from an_interesting_site import settings
-from reviews.models import Review
-from reviews.calculate_reviews import CalculateRating
-
-form = ProductForm()
 
 
 def products(request):
     ''' A view to show all products '''
-    sort = 'title'
-    products = Products.objects.all().order_by(sort)
+    # Initial sorting
+    sort = 'rate'
+    # Initial number of products to show
+    num_of_products = 15
+    # List of products
+    product_list = Products.objects.all().order_by(sort)[:15]
+    # List of all categories
     category_list = Category.objects.all()
+    # Initialising variables
     selected = None
     search = None
     username = None
@@ -23,58 +27,49 @@ def products(request):
     users_liked_products = []
 
     if request.POST:
+        # Category drop down form handling
         if 'category' in request.POST:
             selected_option = str(request.POST['category'])
-            if selected_option == 'electronics':
-                categories = 'electronics'
-                selected = categories
-            elif selected_option == 'clothing':
+            if selected_option == 'clothing':
                 categories = ['mens_clothing', 'womens_clothing']
                 selected = categories
             elif selected_option == 'home_garden':
                 categories = ['home', 'garden']
                 selected = categories
-            elif selected_option == 'jewelery':
-                categories = str(selected_option)
+            else:
+                categories = selected_option
                 selected = categories
 
-            products = products.filter(category__name__in=categories)
-        if 'all_products_z' in request.POST:
-            sort = '-title'
-            products = Products.objects.all().order_by(sort)
+            # Shows list of all
+            product_list = Products.objects.filter(category__name__in=categories)
 
-    for product in products:
+    for product in product_list:
         CalculateRating(product_id=product.pk)
 
     if user.is_authenticated:
         username = request.user.username
-        for like in likes:
-            if like.user.id == user.id:
-                users_liked_products.append(like.product.pk)
+        for item in likes:
+            if item.user.id == user.id:
+                users_liked_products.append(item.product.pk)
 
     if 'author' in request.GET:
         author = request.GET['author']
-        products = products.filter(author__in=author)
+        product_list = product_list.filter(author__in=author)
 
     if request.GET:
-        if 'category' in request.GET:
-            categories = request.GET.getlist('category')
-            products = products.filter(category__name__in=categories)
-            selected = ', '.join(categories)
-
         if 'q' in request.GET:
             search = request.GET['q'].strip()
             results = Q(title__icontains=search) | Q(
                 category__name__icontains=search)
-            products = products.filter(results)
+            product_list = product_list.filter(results)
             selected = str(search)
 
         if 'sort' in request.GET:
             sort = request.GET['sort']
-            products = Products.objects.all().order_by(sort)
+            product_list = Products.objects.all().order_by(sort)
 
     context = {
-        'products': products,
+        'products': product_list,
         'categories': category_list,
         'selected': selected,
         'sort': sort,
@@ -92,6 +87,7 @@ def products(request):
 
 def productdetails(request, product_id):
     ''' A view to return details of the specified product '''
+    form = ProductForm()
     product = get_object_or_404(Products, pk=product_id)
     price = product.price
     reviews = Review.objects.filter(product_id=product_id)
@@ -110,12 +106,9 @@ def add_product(request):
     A view to return a create and render the add product form
     and handle users response
     '''
-    categories_with_sizes = settings.categories_with_sizes
-    print(categories_with_sizes)
     user = request.user
     user_id = None
     username = None
-    form = ProductForm()
     file = 'No image'
     categories = Category.objects.all()
     category_list = []
@@ -148,8 +141,11 @@ def add_product(request):
 
             author = User(user_id)
 
-            new_product = Products(title=title, image=image, category=category_input, price=price, description=description, rate=rate, count=count,
-                                   has_sizes=has_sizes, author=author)
+            new_product = Products(
+                            title=title, image=image,
+                            category=category_input, price=price,
+                            description=description, rate=rate,
+                            count=count, has_sizes=has_sizes, author=author)
 
             new_product.save()
 
@@ -160,22 +156,23 @@ def add_product(request):
         'category_list': category_list,
         'categories': categories,
         'user_id': user_id,
-        'categories_with_sizes': categories_with_sizes,
     }
 
     return render(request, 'products/add_product.html', context)
 
 
 def like(request, product_id, user_id):
-    ''' A view to like a product '''
+    ''' Likes a product '''
     product = Products(product_id)
     user = User(user_id)
     new_like = Likes(product=product, user=user)
     new_like.save()
-    return redirect('products')
+    return redirect(request, 'products')
 
 
 def unlike(request, product_id, user_id):
-    like = Likes.objects.filter(user=User(user_id)).filter(product=Products(product_id))
-    like.delete()
-    return redirect('products')
+    ''' Unlikes a product '''
+    item = Likes.objects.filter(
+        user=User(user_id)).filter(product=Products(product_id))
+    item.delete()
+    return redirect(request, 'products')
